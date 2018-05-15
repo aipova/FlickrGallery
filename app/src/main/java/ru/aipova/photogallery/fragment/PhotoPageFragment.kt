401d92998894d637e3a5bdf0059541833985edfb
aipova <aipova.a@gmail.com>
2018-05-15 15:10:11 +0300

    web view - add action view if not http

--- app/src/main/java/ru/aipova/photogallery/fragment/PhotoPageFragment.kt
+++ app/src/main/java/ru/aipova/photogallery/fragment/PhotoPageFragment.kt
@@ -1,7 +1,10 @@
 package ru.aipova.photogallery.fragment
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
@@ -10,6 +13,7 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ProgressBar
@@ -38,7 +42,7 @@ class PhotoPageFragment : VisibleFragment() {
         with(webView) {
             settings.javaScriptEnabled = true
             webChromeClient = webChromeClientImpl
-            webViewClient = WebViewClient()
+            webViewClient = webViewClientImpl
             loadUrl(uri.toString())
         }
 
@@ -60,6 +64,31 @@ class PhotoPageFragment : VisibleFragment() {
         }
     }
 
+    private val webViewClientImpl = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            return handleRequest(Uri.parse(url))
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            return handleRequest(request?.url)
+        }
+
+        fun handleRequest(uri: Uri?): Boolean {
+            return if (HTTP_SCHEMES.contains(uri?.scheme)) {
+                false
+            } else {
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+                true
+            }
+
+        }
+    }
+
     fun canGoBack(): Boolean {
         return webView.canGoBack()
     }
@@ -71,6 +100,7 @@ class PhotoPageFragment : VisibleFragment() {
     companion object {
         private const val ARG_URI = "photo_page_uri"
         private const val MAX_PROGRESS = 100
+        private val HTTP_SCHEMES = listOf("http", "https")
 
         fun newInstance(uri: Uri) : PhotoPageFragment {
             val args = Bundle().apply { putParcelable(ARG_URI, uri) }
